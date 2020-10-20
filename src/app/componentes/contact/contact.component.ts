0d732ddb94397d7cfa6f58fcec245f76964c4e2f
ErikGaston <erikmartinez_cba@hotmail.com>
2020-10-20 12:54:51 -0300

    form contact

--- src/app/componentes/contact/contact.component.ts
+++ src/app/componentes/contact/contact.component.ts
@@ -1,15 +1,24 @@
 import { Component, OnInit } from '@angular/core';
-
+import {ContactoUsuario} from '../../modelo/contacto.usuario';
 @Component({
   selector: 'app-contact',
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
 export class ContactComponent implements OnInit {
+  public title: string;
+  public contacto: ContactoUsuario;
 
-  constructor() { }
+  constructor() { 
+    this.title = "Escribinos...";
+    this.contacto = new ContactoUsuario("","","","");
+  }
 
   ngOnInit(): void {
+  }
+
+  onSubmit(form){
+    console.log(this.contacto);
   }
 
 }
--- src/app/modelo/contacto.usuario.ts
+++ src/app/modelo/contacto.usuario.ts
@@ -1,9 +1,9 @@
 export class ContactoUsuario{
     constructor(
-        public nombre:string,
-        public apellido:string,
+        public name:string,
+        public telephone:string,
         public email:string,
-        public mensaje:string){
+        public message:string){
             
         }
 }
